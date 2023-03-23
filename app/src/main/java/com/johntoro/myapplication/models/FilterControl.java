65d6667c3add566ad05b330e123929683563df28
honjoo <64689788+Teddies1@users.noreply.github.com>
2023-03-23 13:51:38 +0800

    created FilterControl

---
+++ app/src/main/java/com/johntoro/myapplication/models/FilterControl.java
@@ -0,0 +1,32 @@
+package com.johntoro.myapplication.models;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class FilterControl {
+    private boolean isActiveHours;
+    private boolean isRating;
+    public FilterControl(){
+        this.isActiveHours = false;
+        this.isRating = false;
+    }
+    // when click on filter by rating, set this to true
+    public void setActiveHours(boolean activeHours) {
+        isActiveHours = activeHours;
+    }
+
+    public void setRating(boolean rating) {
+        isRating = rating;
+    }
+
+    public void sort(List<Results> res){
+        if (isActiveHours){
+            //res.sort(Comparator.comparing(Results::getOpeningHours));
+        }
+        if (isRating){
+            res.sort(Comparator.comparing(Results::getRating));
+        }
+    }
+
+}
