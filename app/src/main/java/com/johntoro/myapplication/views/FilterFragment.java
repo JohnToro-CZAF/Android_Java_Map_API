a65feff05b0b32fbc92b71e97cedd8b5706f8bfe
Phan Nhat Hoang <johnphan1608@gmail.com>
2023-04-02 12:31:26 +0800

    Fixed FilterFragment

--- app/src/main/java/com/johntoro/myapplication/views/FilterFragment.java
+++ app/src/main/java/com/johntoro/myapplication/views/FilterFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.johntoro.myapplication.R;
 import com.johntoro.myapplication.models.FilterControl;
 import com.johntoro.myapplication.models.Results;
 
